a21856c7552c27b055b4411c19d4b3183f4eca34
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-09-18 17:04:34 -0400

    Claculador de distancia luna tierra 2.0v

--- calculador_distancia_luna.py
+++ calculador_distancia_luna.py
@@ -1,4 +1,4 @@
 distancia_tierra_luna = 384400
-distancia_recorrida = 50000
+distancia_recorrida = int(input("Pro favor, ingrese duistancia recorrida: "))
 distancia_restante = distancia_tierra_luna - distancia_recorrida
-print("Nos falta recorrer: " + str(distancia_restante))
+print("Nos falta recorrer: ", (distancia_restante))
