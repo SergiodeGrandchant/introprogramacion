ae0884d95478eb27efc9d224b30775ba40fdc4bf
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-09-25 16:56:57 -0400

    E2: Asignar un mensaje de texto a una variable y mostrar el valor de la variable en pantalla. Luego, asignar otro mensaje a la variable y mostrar la variable en pantalla.

---
+++ variables/ejercicio2.py
@@ -0,0 +1,5 @@
+#E2: Asignar un mensaje de texto a una variable y mostrar el valor de la variable en pantalla. Luego, asignar otro mensaje a la variable y mostrar la variable en pantalla.
+var1 = "Frase 1"
+print(var1)
+var1 = "segunda frase"
+print(var1)
