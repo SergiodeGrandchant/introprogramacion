f17afcdc1211ed3acbb7dca7c2fece0a0587bef5
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-09-25 16:58:05 -0400

    E4: Imprimir el siguiente mensaje, tal como se muestra, en la pantalla: Albert Einstein dijo una vez: “Cada día sabemos más y entendemos menos

---
+++ variables/ejercicio4.py
@@ -0,0 +1,3 @@
+#E4: Imprimir el siguiente mensaje, tal como se muestra, en la pantalla: Albert Einstein dijo una vez: “Cada día sabemos más y entendemos menos”
+frase = "Albert Einstein dijo una vez: Cada día sabemos más y entendemos menos"
+print(frase)
