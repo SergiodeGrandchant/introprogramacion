0595dc7a025a1999e8cc887b2d565ae5c5c0c1b2
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-09-25 16:57:34 -0400

    E3: Leer el nombre del usuario del teclado y mostrar un mensaje de bienvenida en la pantalla. Ejemplo: Hola, Paul. Bienvenido a Python!

---
+++ variables/ejercicio3.py
@@ -0,0 +1,3 @@
+# E3:Leer el nombre del usuario del teclado y mostrar un mensaje de bienvenida en la pantalla. Ejemplo:Hola, Paul. Bienvenido a Python!
+nombre = input("¿Cual es tu nombre?")
+print("Hola, ",nombre, ". Bienvenido a Python")
