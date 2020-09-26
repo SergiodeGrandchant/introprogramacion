26c334f2cbd0307b2bf7a58061e63b938a659480
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-09-26 10:40:50 -0400

    E6: Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, y muestre por pantalla la frase: Tu índice de masa corporal es <valor_de_imc_va_aquí>

---
+++ variables/ejercicio6.py
@@ -0,0 +1,6 @@
+#E6: Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, y muestre por pantalla la frase: Tu índice de masa corporal es <valor_de_imc_va_aquí>
+peso = float(input("¿Cual es tu peso? pon tu peso en kg: "))
+altura = float(input("¿Cual es tu altura? ponla en metros porfavor: "))
+# idmc = indice de masa corporal
+idmc = peso / (altura**2)
+print("tu indice de masa corporal es de: ", idmc)
