7d0073579c9acc28f2de7ca4594bd18c7759ad42
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-09-26 10:43:08 -0400

    E1: Asignar un mensaje de texto a una variable y mostrar el valor de la variable en pantalla

--- variables/ejercicio1.py
+++ variables/ejercicio1.py
@@ -1,3 +1,3 @@
-# E1: Asignar un mensaje de texto a una variable y mostrar el valor de la variable en pantalla
+#E1: Asignar un mensaje de texto a una variable y mostrar el valor de la variable en pantalla
 var1 = "hola mundo"
 print(var1)
