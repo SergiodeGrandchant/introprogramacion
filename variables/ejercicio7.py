2c8ce27f5a1f5549441dd2f2553e56c09efca5a9
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-09-27 17:55:44 -0400

    E7: Leer un número entero “n” del teclado y después mostrar en pantalla la suma de todos los enteros desde 1 hasta “n”. La suma de los primeros enteros positivos puede ser calculada de la siguiente forma

---
+++ variables/ejercicio7.py
@@ -0,0 +1,4 @@
+# E7: Leer un número entero “n” del teclado y después mostrar en pantalla la suma de todos los enteros desde 1 hasta “n”. La suma de los primeros enteros positivos puede ser calculada de la siguiente forma:
+n = float(input("Dame un nùmero "))
+suma = float((n*(n+1))/2)
+print(suma)
