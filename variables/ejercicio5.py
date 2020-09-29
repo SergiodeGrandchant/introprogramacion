fbffefdd4af6d34f39898ca9f567c26aee948106
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-09-29 17:22:17 -0400

    E5: Escribir un programa que pregunte al usuario por el número de horas trabajadas y la paga por hora. Después mostrar en la pantalla el monto a pagar al trabajador

--- variables/ejercicio5.py
+++ variables/ejercicio5.py
@@ -1,4 +1,5 @@
 #E5: Escribir un programa que pregunte al usuario por el número de horas trabajadas y la paga por hora. Después mostrar en la pantalla el monto a pagar al trabajador
-horas = float(input("¿Cuantas horas al dia trabajas?"))
+# ht = horas trabajadas
+ht = float(input("¿Cuantas horas al dia trabajas?"))
 pago = float(input("¿Cuanto te pagan por hora trabajada?"))
-print(horas * pago)
+print(ht * pago)
