e21c1875504c4c88ecff3a707d38d6c31a3563c3
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-09-30 19:16:36 -0400

    E9: Leer el día, mes y año de nacimiento del usuario de la entrada y mostrar un mensaje indicando si el usuario es mayor de 18 años o no

--- variables/ejercicio9.py
+++ variables/ejercicio9.py
@@ -1,11 +1,12 @@
 # E9: Leer el día, mes y año de nacimiento del usuario de la entrada y mostrar un mensaje indicando si el usuario es mayor de 18 años o no
-cumple = str(input("¿cuando es tu cumpleaños? "))
+cumple = input("¿cuando es tu cumpleaños? ")
 # a = años cuando nacio
-a = input("¿En que año naciaste? ")
+a = int(input("¿En que año naciaste? "))
 # aa = año actual
-aa = input("¿En que año estas? ")
-edad = aa - a
+aa = int(input("¿En que año estas? "))
+edad = (aa-a)
 
-    if edad >= 18:
-        print("El usuario en mayor de edad")
-    else:print("el usuario es menor de edad")
+if edad >= 18:
+    print("El usuario en mayor de edad con ", edad, "años")
+else:
+    print("el usuario es menor de e edad con ", edad, "años")
