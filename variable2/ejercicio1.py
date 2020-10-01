1a465a3263422afedef434f430355e218702878d
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-10-01 16:23:54 -0400

    E1: Leer un número por teclado que representa una longitud en millas y mostrar el valor equivalente en kilómetros en pantalla. El mensaje debe tener el siguiente formato: “<longitud_millas> millas equivalen a <longitud_kilometros> kilómetros”

--- variable2/ejercicio1.py
+++ variable2/ejercicio1.py
@@ -1,4 +1,4 @@
 # E1: Leer un número por teclado que representa una longitud en millas y mostrar el valor equivalente en kilómetros en pantalla. El mensaje debe tener el siguiente formato: “<longitud_millas> millas equivalen a <longitud_kilometros> kilómetros”
-millas = float(input("dame la longitud en millas"))
-km = float(millas * 1.60934)
+millas = float(input("dame la longitud en millas: "))
+km = millas * 1.60934
 print(millas, "millas equivalen a: ", km, "kilometros")
