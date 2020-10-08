0f70b448a061b10b0cf44bd17b60dc6d9d2df80a
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-10-08 12:27:09 -0400

    E1: Escribir un programa que pida al usuario ingresar un número positivo, mientras el número ingresado no sea positivo, mostrar un mensaje de error y pedir al usuario que vuela a ingresarun número

--- flujo_ciclico/ejercicio1.py
+++ flujo_ciclico/ejercicio1.py
@@ -1,6 +1,6 @@
 #E1: Escribir un programa que pida al usuario ingresar un número positivo, mientras el número ingresado no sea positivo, mostrar un mensaje de error y pedir al usuario que vuela a ingresarun número.
 num = float(input("Dame un numero positivo "))
-while num<=0:
+while num <= 0:
     print("Error valor invalido ")
     num = float(input("Dame un numero positivo"))
 print("Fin")
