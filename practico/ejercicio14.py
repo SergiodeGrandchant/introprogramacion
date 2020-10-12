7dce7b041dd9362dd9a289b3764b9cbc2e0da698
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-10-12 11:16:08 -0400

    .

--- practico/ejercicio14.py
+++ practico/ejercicio14.py
@@ -1,1 +1,15 @@
 # https://bit.ly/36LtifW
+edad = int(input("dame tu eded: "))
+altura = float(input("Dame tu altura en metros: "))
+peso = float(input("Dame tu peso en kilos: "))
+indice = peso/(altura**2)
+if indice < 22.00:
+    if edad <45:
+        print("Factor de riesgo bajo")
+    else:
+        print("Factor de riesgo medio")
+if indice >= 22.00:
+    if edad < 45:
+        print("Factor de riesgo medio")
+    else:
+        print("Factor de riesgo alto")
