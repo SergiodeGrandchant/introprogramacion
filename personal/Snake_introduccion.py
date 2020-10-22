f354123d9f0c9e0a51e5ea56f3ede750c996dc26
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-10-22 16:11:17 -0400

    .

--- personal/Snake_introduccion.py
+++ personal/Snake_introduccion.py
@@ -3,7 +3,13 @@ import pygame
 import sys
 from pygame.locals import *
 
-ventana = pygame.display.set_mode((600, 600))
+ventana = pygame.display.set_mode((600, 500))
+pluma = turtle.Turtle()
+
+pluma.speed(5)
+for i in range(0, 4):
+    pluma.forward(100)
+    pluma.left(90)
 
 
 while True:
