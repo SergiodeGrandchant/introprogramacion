0e2895330bdd97f39ba0d05c2c674c203abf040d
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-10-23 09:41:22 -0400

    Identificar la vocal

--- practico funciones/ejercicio4.py
+++ practico funciones/ejercicio4.py
@@ -1,9 +1,10 @@
+# Escribir una función que tome un carácter y devuelva True si es una vocal, de lo contrario devuelve False.
 
 def vocal(letra):
     if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
-        print(letra, " = es una vocal")
+        print(letra, " = TRUE")
     else:
-        print(letra, " = es una consonante")
+        print(letra, " = FALSE")
 
 letra = input("Dame una letra: ")
 vocal(letra)
