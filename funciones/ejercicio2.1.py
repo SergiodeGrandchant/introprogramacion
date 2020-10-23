6ea5d52ee4b1a39fafc0c036684eda34e5c3c31e
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-10-23 09:48:33 -0400

    .

--- funciones/ejercicio2.1.py
+++ funciones/ejercicio2.1.py
@@ -1,7 +1,7 @@
 #Tabla del 1 - 10
 def tablaM():
     for j in range(1, 11):
-        print("Tabla del numero: ", j)
+        print("Tabla del numero:", j)
         for i in range(1, 11):
             print(j, " X ", i, " = ", i * j)
 
