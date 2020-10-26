4fa3dbedc968c97d551a263237c3ec257ebebf1d
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-10-26 18:35:29 -0400

    len

---
+++ practico funciones/ejercicioe pero chill.py
@@ -0,0 +1,5 @@
+def letras(palabra):
+    print("La palabra dada \" ", palabra, " \" consta de ", len(palabra), " letras")
+
+palabra = input("dame la palabra que quieres que contemos: ")
+letras(palabra)
