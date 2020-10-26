d3d8714b75e4f1260bd259e89c373c88f19cfe85
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-10-26 18:49:50 -0400

    palabra inversa

--- practico funciones/ejercicio5.py
+++ practico funciones/ejercicio5.py
@@ -1,7 +1,6 @@
 #5- Definir una función inversa() que calcule la inversión de una cadena. Por ejemplo la cadena "estoy probando" debería devolver la cadena "odnaborp yotse"
 def inversa(palabra):
     tam = len(palabra)
-    palabraInv = ""
     for indice in range(tam -1,-1,-1):
         palabraInv = palabraInv + palabra[indice]
     print(palabraInv)
