abc8ace6ebd6a3ad95877ec3158aeef1063b9614
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-11-24 15:04:02 -0400

    factorial

---
+++ general/ejercicio FACT.py
@@ -0,0 +1,14 @@
+num = int(input("Dame el numero al que quieras sacar el factorial: "))
+factorial = []
+for i in range(1,20+1):
+    numres = str(i) + "!"
+    factorial.append(numres)
+print(factorial)
+
+mul = 1
+while num > 1:
+    mul = num * mul
+    num = num-1
+    print("El FACT DE ", num, " es: ", mul)
+
+
