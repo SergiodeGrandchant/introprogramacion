ca416eae326d5690c7ff43f68f2c0dc9fa2bdf5a
SERGIO <sergio.degrandchant@ucb.edu.bo>
2020-11-24 16:35:31 -0400

    suma de vectores

--- general/suma de vectores.py
+++ general/suma de vectores.py
@@ -1,19 +1,24 @@
 vec1 = []
 vec2 = []
+vecS = []
 suma1 = 0
 suma2 = 0
 for i in range (1, 6):
     i = str(i)
     num = int(input(f"Dame le numero Nº{i} para el primer vector: "))
     vec1.append(num)
-    suma1 = suma1 + num
 print("-"*50)
 
 for j in range (1, 6):
     j = str(j)
     num2 = int(input(f"Dame le numero Nº{j} para el segundo vector: "))
     vec2.append(num2)
-    suma2 = suma2 + num2
 
-res = suma1 + suma2
-print("La suam de los vectores es de: ", res)
+for ñ in range (0, 5):
+    suma = vec1[ñ] + vec2[ñ]
+    vecS.append(suma)
+
+
+print(vec1)
+print(vec2)
+print(vecS)
